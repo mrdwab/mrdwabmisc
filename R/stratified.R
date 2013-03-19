42acceb80bd12d34ef3f5295c399c0cf60d25d2f
Ananda Mahto <mrdwab@gmail.com>
2013-03-19 16:56:11 +0530

    Fixed typo

--- R/stratified.R
+++ R/stratified.R
@@ -131,7 +131,7 @@ stratified <- function(df, group, size, select = NULL, seed = NULL, ...) {
     df <- df
   } else {
     if (is.null(names(select))) stop("'select' must be a named list")
-    if (!all(names(selectme) %in% names(dat1))) 
+    if (!all(names(select) %in% names(dat1))) 
       stop("Please verify your 'select' argument")
     temp <- sapply(names(select), 
                    function(x) df[[x]] %in% select[[x]])
