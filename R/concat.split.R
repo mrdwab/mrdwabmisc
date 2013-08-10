7569e574401db30b045e4f00af0a309e1099dc28
Ananda Mahto <mrdwab@gmail.com>
2013-08-10 23:15:05 +0530

    code edited and drop.col changed to drop in all instances

--- R/concat.split.R
+++ R/concat.split.R
@@ -26,20 +26,16 @@
 #' \dontshow{rm(temp)}
 #' 
 #' @export concat.split.compact
-concat.split.compact <- function(data, split.col, sep = ",", drop = FALSE, fixed = NULL) {
-  if (!isTRUE(is.character(data[split.col]))) a <- as.character(data[[split.col]])
+concat.split.compact <- function(data, split.col, sep = ",", 
+                                 drop = FALSE, fixed = NULL) {
+  if (!is.character(data[split.col])) a <- as.character(data[[split.col]])
   else a <- data[[split.col]]
   
   t1 <- read.concat(a, names(data[split.col]), sep)
   if (isTRUE(drop)) cbind(data[othernames(data, split.col)], t1)
   else cbind(data, t1)
 }
-
-
-
-
-
-
+NULL
 
 #' Split numeric concatenated values into their corresponding column position
 #' 
@@ -47,7 +43,7 @@ concat.split.compact <- function(data, split.col, sep = ",", drop = FALSE, fixed
 #' \code{data.frame}.
 #' 
 #' 
-#' @param data The source \code{data.frame}
+#' @param data The source \code{data.frame}.
 #' @param split.col The variable that needs to be split (either name or index
 #' position).
 #' @param sep The character separating each value. Can also be a regular
@@ -66,7 +62,7 @@ concat.split.compact <- function(data, split.col, sep = ",", drop = FALSE, fixed
 #' selected, the function calls \code{\link{valueMat}}.
 #' @author Ananda Mahto
 #' @seealso \code{\link{concat.split}}, \code{\link{concat.split.list}},
-#' \code{\link{concat.split.expanded}}, \code{\link{concat.split.multiple}}, \code{\link{binaryMat}},
+#' \code{\link{concat.split.compact}}, \code{\link{concat.split.multiple}}, \code{\link{binaryMat}},
 #' \code{\link{valueMat}}
 #' @examples
 #' 
@@ -83,7 +79,7 @@ concat.split.compact <- function(data, split.col, sep = ",", drop = FALSE, fixed
 #' @export concat.split.expanded
 concat.split.expanded <- function(data, split.col, sep = ",", mode = NULL, 
                                   drop = FALSE, fixed = FALSE, fill = NA) {
-  if (!isTRUE(is.character(data[split.col]))) a <- as.character(data[[split.col]])
+  if (!is.character(data[split.col])) a <- as.character(data[[split.col]])
   else a <- data[[split.col]]
   
   b <- strsplit(a, sep, fixed = fixed)
@@ -91,8 +87,10 @@ concat.split.expanded <- function(data, split.col, sep = ",", mode = NULL,
   if (suppressWarnings(is.na(try(max(as.numeric(unlist(b))))))) {
     temp1 <- concat.split.compact(data, split.col =  split.col, sep = sep, 
                                   drop = drop, fixed = fixed)
-    message("Your concatenated variable is a string.
-            We have used 'concat.split.compact' instead.")
+    m <- paste("Your concatenated variable is a string.", 
+               "We have used 'concat.split.compact' instead.", sep = "\n")
+    message(m)
+    Return <- "A"
   } else if (!is.na(try(max(as.numeric(unlist(b)))))) {
     if (is.null(mode)) mode = "binary"
     nchars <- max(nchar(unlist(b)))
@@ -111,18 +109,16 @@ concat.split.expanded <- function(data, split.col, sep = ",", mode = NULL,
         temp
       },
       stop("'mode' must be 'binary' or 'value'"))
+    expandedNames <- colnames(temp1)
     temp1 <- cbind(data, temp1)
+    Return <- "B"
   }
   
-  if (isTRUE(drop)) temp1[othernames(data, split.col)]
+  if (isTRUE(drop) & Return == "B") temp1[c(othernames(data, split.col), 
+                                            expandedNames)]
   else temp1
 }
-
-
-
-
-
-
+NULL
 
 #' Split concatenated cells in a \code{data.frame} into a \code{list} format
 #' 
@@ -139,9 +135,9 @@ concat.split.expanded <- function(data, split.col, sep = ",", mode = NULL,
 #' \code{FALSE}.
 #' @param fixed Used for \code{\link{strsplit}} for allowing regular
 #' expressions to be used.
-#' @return A \code{data.frame}
+#' @return A \code{data.frame} with the concatenated column split and added as a \code{list}.
 #' @author Ananda Mahto
-#' @seealso \code{\link{concat.split}}, \code{\link{concat.split.condensed}},
+#' @seealso \code{\link{concat.split}}, \code{\link{concat.split.compact}},
 #' \code{\link{concat.split.expanded}}, \code{\link{concat.split.multiple}}
 #' @examples
 #' 
@@ -155,7 +151,7 @@ concat.split.expanded <- function(data, split.col, sep = ",", mode = NULL,
 #' @export concat.split.list
 concat.split.list <- function(data, split.col, sep = ",", 
                               drop = FALSE, fixed = FALSE) {
-  if (!isTRUE(is.character(data[split.col]))) a <- as.character(data[[split.col]])
+  if (!is.character(data[split.col])) a <- as.character(data[[split.col]])
   else a <- data[[split.col]]
   
   varname <- paste(names(data[split.col]), "list", sep="_")
@@ -171,13 +167,7 @@ concat.split.list <- function(data, split.col, sep = ",",
   if (isTRUE(drop)) data[othernames(data, split.col)]
   else data
 }
-
-
-
-
-
-
-
+NULL
 
 #' Split concatenated cells in a \code{data.frame}
 #' 
@@ -197,29 +187,28 @@ concat.split.list <- function(data, split.col, sep = ",",
 #' \code{"|"}, you can set \code{sep = ",|;|\|"} or \code{sep = "[,;|]"}, and
 #' \code{fixed = FALSE} to split on any of those characters.}
 #' 
-#' @param data The source data.frame
+#' @param data The source \code{data.frame}.
 #' @param split.col The variable that needs to be split; can be specified
 #' either by the column number or the variable name.
-#' @param sep The character separating each value (defaults to ",").
-#' @param structure Can be either "compact", "expanded", or "list". Defaults to
-#' "compact". See Details.
-#' @param mode Can be either binary or value (where binary is default and it
-#' recodes values to 1 or NA, like Boolean data, but without assuming 0 when
-#' data is not available).  This setting only applies when structure =
-#' "expanded"; a warning message will be issued if used with other structures.
-#' @param drop.col Logical (whether to remove the original variable from the
+#' @param sep The character separating each value (defaults to \code{","}).
+#' @param structure Can be either \code{"compact"}, \code{"expanded"}, or \code{"list"}. Defaults to
+#' \code{"compact"}. See Details.
+#' @param mode Can be either \code{"binary"} or \code{"value"} (where \code{"binary"} is default and it
+#' recodes values to 1 or \code{NA}, like Boolean data, but without assuming 0 when
+#' data is not available).  This setting only applies when \code{structure =
+#' "expanded"}; a warning message will be issued if used with other structures.
+#' @param drop Logical (whether to remove the original variable from the
 #' output or not). Defaults to \code{FALSE}.
 #' @param fixed Is the input for the \code{sep} value \emph{fixed}, or a
 #' \emph{regular expression}? See Details.
-#' @note If using \code{structure = "compact"}, the value for \code{sep} can
-#' only be a single character. See the "Advanced Usage" example of how to
-#' specify multiple characters for batch conversion of columns.
+#' @param fill The "fill" value for missing values when \code{structure = "expanded"}. Defaults to \code{NA}.
+#' @note This is more of a "legacy" or "convenience" wrapper function encompassing the features available in the separated functions of \code{\link{concat.split.compact}}, \code{\link{concat.split.list}}, and \code{\link{concat.split.expanded}}. 
 #' @author Ananda Mahto
-#' @seealso \code{\link{concat.split.condensed}},
+#' @seealso \code{\link{concat.split.compact}},
 #' \code{\link{concat.split.expanded}}, \code{\link{concat.split.list}},
 #' \code{\link{concat.split.multiple}}
 #' @references \itemize{ \item See
-#' \url{http://stackoverflow.com/q/10100887/1270695} \item The
+#' \url{http://stackoverflow.com/q/10100887/1270695} for some history of this function, even though the solution is not used at all here. \item The
 #' \code{"condensed"} setting was inspired by an answer from David Winsemius to
 #' a question at Stack Overflow.  See:
 #' \url{http://stackoverflow.com/a/13924245/1270695} }
@@ -232,34 +221,34 @@ concat.split.list <- function(data, split.col, sep = ",",
 #' concat.split(temp, 2)
 #' 
 #' # ... or by name, and drop the offensive first column
-#' concat.split(temp, "Likes", drop.col = TRUE)
+#' concat.split(temp, "Likes", drop = TRUE)
 #' 
 #' # The "Hates" column uses a different separator
-#' concat.split(temp, "Hates", sep = ";", drop.col = TRUE)
+#' concat.split(temp, "Hates", sep = ";", drop = TRUE)
 #' 
 #' \dontrun{
 #' # You'll get a warning here, when trying to retain the original values
-#' concat.split(temp, 2, mode = "value", drop.col = TRUE)
+#' concat.split(temp, 2, mode = "value", drop = TRUE)
 #' }
 #' 
 #' # Try again. Notice the differing number of resulting columns
 #' concat.split(temp, 2, structure = "expanded",
-#' mode = "value", drop.col = TRUE)
+#' mode = "value", drop = TRUE)
 #' 
 #' # Let's try splitting some strings... Same syntax
-#' concat.split(temp, 3, drop.col = TRUE)
+#' concat.split(temp, 3, drop = TRUE)
 #' 
 #' # Split up the "Likes column" into a list variable; retain original column
-#' head(concat.split(concat.test, 2, structure = "list", drop.col=FALSE))
+#' head(concat.split(concat.test, 2, structure = "list", drop = FALSE))
 #' 
 #' # View the structure of the output to verify
 #' # that the new column is a list; note the
 #' # difference between "Likes" and "Likes_list".
-#' str(concat.split(temp, 2, structure = "list", drop.col=FALSE))
+#' str(concat.split(temp, 2, structure = "list", drop = FALSE))
 #' 
 #' @export concat.split
 concat.split <- function(data, split.col, sep = ",", structure = "compact",
-                         mode = NULL, drop.col = FALSE, fixed = FALSE, fill = NA) {
+                         mode = NULL, drop = FALSE, fixed = FALSE, fill = NA) {
   
   Message <- paste(c("", "'mode' supplied but ignored.", 
                      "'mode' setting only applicable",
@@ -268,39 +257,35 @@ concat.split <- function(data, split.col, sep = ",", structure = "compact",
     structure, 
     compact = {
       if (!is.null(mode)) warning(Message)
-      concat.split.compact(data = data, split.col = split.col, sep = sep,
-                           drop = drop.col, fixed = fixed)
+      concat.split.compact(data = data, split.col = split.col, 
+                           sep = sep, drop = drop, fixed = fixed)
     },
     list = {
       if (!is.null(mode)) warning(Message)
-      concat.split.list(data = data, split.col = split.col, sep = sep,
-                        drop = drop.col, fixed = fixed)
+      concat.split.list(data = data, split.col = split.col, 
+                        sep = sep, drop = drop, fixed = fixed)
     },
     expanded = {
-      concat.split.expanded(data = data, split.col = split.col, sep = sep, 
-                            mode = mode, drop = drop.col, fixed = fixed, fill = fill)
+      concat.split.expanded(data = data, split.col = split.col, 
+                            sep = sep, mode = mode, drop = drop, 
+                            fixed = fixed, fill = fill)
     },
     stop("'structure' must be either 'compact', 'expanded', or 'list'"))
   temp
 }
-
-
-
-
-
-
+NULL
 
 #' Split concatenated cells in a \code{data.frame} and optionally reshape the output
 #' 
-#' This is an extended version of the \code{\link{concat.split.condensed}} function that allows the user to split multiple columns at once and optionally use the \code{\link{Reshape}} function to convert the \code{data.frame} into a "long" format.
+#' This is an extended version of the \code{\link{concat.split.compact}} function that allows the user to split multiple columns at once and optionally use the \code{\link{Reshape}} function to convert the \code{data.frame} into a "long" format.
 #' 
 #' @param data The source \code{data.frame}.
 #' @param split.cols A vector of columns that need to be split.
 #' @param seps A vector of the separator character used in each column. If all columns use the same character, you can enter that single character.
-#' @param direction The desired form of the resulting \code{data.frame}, either \code{'wide'} or \code{'long'}.
-#' @return A \code{data.frame}
+#' @param direction The desired form of the resulting \code{data.frame}, either \code{'wide'} or \code{'long'}. Defaults to \code{'wide'}.
+#' @return A \code{data.frame}. If \code{direction = "long"}, a \code{data.frame} with additional attributes created by the \code{\link{reshape}} function in base R.
 #' @author Ananda Mahto
-#' @seealso \code{\link{concat.split}}, \code{\link{concat.split.condensed}},
+#' @seealso \code{\link{concat.split}}, \code{\link{concat.split.compact}},
 #' \code{\link{concat.split.expanded}}, \code{\link{concat.split.multiple}}, \code{\link{Reshape}}
 #' @examples
 #' 
@@ -311,7 +296,8 @@ concat.split <- function(data, split.col, sep = ",", structure = "compact",
 #'                       seps = ",", direction = "long")
 #' \dontshow{rm(temp)}
 #' 
-concat.split.multiple <- function(data, split.cols, seps = ",", direction = "wide") {
+concat.split.multiple <- function(data, split.cols, seps = ",", 
+                                  direction = "wide") {
   IDs <- othernames(data, split.cols)
   if (is.numeric(split.cols)) split.cols <- names(data)[split.cols]
   if (length(seps) == 1) seps <- rep(seps, length(split.cols))
@@ -336,3 +322,4 @@ concat.split.multiple <- function(data, split.cols, seps = ",", direction = "wid
   out[out == ""] <- NA
   out
 }
+NULL
